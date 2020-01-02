59a8001a8bdd4cfd6f036838c84a6cb2201a90c7
eduardo <efulgenciocomendeiro@gmail.com>
2020-01-02 17:52:34 +0100

    Configure rx for components. And sum values for total fruit.

--- withRxSwift/withRxSwift/ViewController.swift
+++ withRxSwift/withRxSwift/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
@@ -16,10 +18,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblParcialPeras: UILabel!
     @IBOutlet weak var lblParcialManzanas: UILabel!
     @IBOutlet weak var lblParcialPlatanos: UILabel!
-    @IBOutlet weak var lblTotalPeras: UIStackView!
+
+    @IBOutlet weak var lblTotalPeras: UILabel!
     @IBOutlet weak var lblTotalManzanas: UILabel!
     @IBOutlet weak var lblTotalPlatanos: UILabel!
     
+    let disposeBag = DisposeBag()
     
     // Objetivo: introducir numero de peras en txtPeras, actualiza el parcial
     // cuando se pulsa enter en el campo txtPeras el valor de parcial peras
@@ -27,10 +31,70 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        bindTextAndLabelPeras()
+        bindTextAndLabelManzanas()
+        bindTextAndLabelPlatanos()
     }
 
+    func bindTextAndLabelPeras() {
+        
+        // Cuando pulse enter se entenderá que confirma el número de unidades
+        txtPeras.rx.controlEvent([.editingDidEndOnExit]).subscribe { textIn in
+            let suma : Int = Int(self.lblParcialPeras.text!)! + Int(self.lblTotalPeras.text!)!
+            self.lblTotalPeras.text = String(suma)
+            self.txtPeras.rx.text.onNext("0") // Inicializo a 0
+        }.disposed(by: disposeBag)
+    
+        txtPeras.rx.text.map {
+                if $0 == "" {
+                    return "0"
+                } else {
+                    return "\($0!)"
+                }
+            }.bind(to: lblParcialPeras.rx.text).disposed(by: disposeBag)
+
+        lblTotalPeras.rx.observe(String.self, "text").subscribe(onNext: { text in
+              print(text!)
+        }).disposed(by: disposeBag)
+    }
+    
+    func bindTextAndLabelManzanas() {
+            txtManzanas.rx.controlEvent([.editingDidEndOnExit]).subscribe { textIn in
+                let suma : Int = Int(self.lblParcialManzanas.text!)! + Int(self.lblTotalManzanas.text!)!
+                self.lblTotalManzanas.text = String(suma)
+            }.disposed(by: disposeBag)
+        
+            txtManzanas.rx.text.map {
+                    if $0 == "" {
+                        return "0"
+                    } else {
+                        return "\($0!)"
+                    }
+                }.bind(to: lblParcialManzanas.rx.text).disposed(by: disposeBag)
+
+            lblTotalManzanas.rx.observe(String.self, "text").subscribe(onNext: { text in
+                  print(text!)
+            }).disposed(by: disposeBag)
+    }
+    
+    func bindTextAndLabelPlatanos() {
+            txtPlatanos.rx.controlEvent([.editingDidEndOnExit]).subscribe { textIn in
+                let suma : Int = Int(self.lblParcialPlatanos.text!)! + Int(self.lblTotalPlatanos.text!)!
+                self.lblTotalPlatanos.text = String(suma)
+            }.disposed(by: disposeBag)
+        
+            txtPlatanos.rx.text.map {
+                    if $0 == "" {
+                        return "0"
+                    } else {
+                        return "\($0!)"
+                    }
+                }.bind(to: lblParcialPlatanos.rx.text).disposed(by: disposeBag)
+
+            lblTotalPlatanos.rx.observe(String.self, "text").subscribe(onNext: { text in
+                  print(text!)
+            }).disposed(by: disposeBag)
+    }
 
 }
 
